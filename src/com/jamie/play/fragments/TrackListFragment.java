4c82bfd5815ed079fb901aeb5af2cdfab554e070
Jamie Hewland <jhewland@gmail.com>
2013-02-11 18:11:24 +0200

    Open player upon track selection

--- src/com/jamie/play/fragments/TrackListFragment.java
+++ src/com/jamie/play/fragments/TrackListFragment.java
@@ -3,11 +3,11 @@ package com.jamie.play.fragments;
 import java.util.List;
 
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import com.jamie.play.activities.MusicActivity;
 import com.jamie.play.adapters.TrackAdapter;
 import com.jamie.play.service.MusicServiceWrapper;
 import com.jamie.play.service.Track;
@@ -26,8 +26,9 @@ public class TrackListFragment extends ListFragment {
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
  	   	List<Track> list = ((TrackAdapter) getListAdapter()).getTrackList();
- 	   	Log.d("SongList", "Track selected: " + list.get(position).getTitle());
  	   	MusicServiceWrapper.playAll(getActivity(), list, position, false);
+ 	   	
+ 	   	((MusicActivity) getActivity()).getMenuDrawer().openMenu();
     }
 
 }
