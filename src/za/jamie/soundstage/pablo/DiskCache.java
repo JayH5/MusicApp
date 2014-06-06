c1a13430de8432c56e5e80184241589a6da0ac40
Jamie Hewland <jhewland@gmail.com>
2014-06-06 14:07:31 +0200

    Use an LRU cache for the disk cache

--- src/za/jamie/soundstage/pablo/DiskCache.java
+++ src/za/jamie/soundstage/pablo/DiskCache.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.StatFs;
 import android.util.Log;
 
+import com.squareup.okhttp.internal.DiskLruCache;
 import com.squareup.picasso.Cache;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -22,109 +23,187 @@ import za.jamie.soundstage.utils.AppUtils;
 public class DiskCache implements Cache {
 
 	private static final String TAG = "DiskCache";
-	
-	public static final String DEFAULT_CACHE_DIR_NAME = "imagecache";
-	public static final int DEFAULT_COMPRESS_QUALITY = 98;
 
-	private final File mCacheDir;
-	
+    private static final int MIN_SIZE = 5 * 1024 * 1024; // 5MB
+    private static final int MAX_SIZE = 50 * 1024 * 1024; // 50MB
+
+    private static final String DEFAULT_CACHE_DIR_NAME = "imagecache";
+	private static final CompressFormat COMPRESS_FORMAT = CompressFormat.JPEG;
+    private static final int COMPRESS_QUALITY = 95;
+    private static final int INDEX = 0;
+
+    private static final int MESSAGE_CLEAR = 0;
+    private static final int MESSAGE_INIT = 1;
+    private static final int MESSAGE_FLUSH = 2;
+    private static final int MESSAGE_CLOSE = 3;
+
+	private DiskLruCache mCache;
+    private File mCacheDir;
+
 	private final Object mLock = new Object();
-	
+    private boolean mStarting = true;
+
 	public DiskCache(Context context) {
 		this(AppUtils.getAppDir(context, DEFAULT_CACHE_DIR_NAME));
 	}
-	
+
 	public DiskCache(File directory) {
 		mCacheDir = directory;
-	}
-	
-	private boolean createDirIfNeeded() {
-		synchronized (mLock) {
-			if (mCacheDir == null) {
-				return false;
-			}
-			if (!mCacheDir.exists()) {
-				return mCacheDir.mkdirs();
-			}
-			return true;
-		}
+        new CacheAsyncTask().execute(MESSAGE_INIT);
 	}
 
-	@Override
-	public void set(String key, Bitmap bitmap) {		
-		synchronized (mLock) {
-			if (createDirIfNeeded()) {
-				final String diskKey = hashKeyForDisk(key);
-				File file = new File(mCacheDir, diskKey);
-				if (file.exists()) { // Already have file saved
-					return;
-				}
-				OutputStream out = null;
-				try {
-					out = new FileOutputStream(file);
-					bitmap.compress(CompressFormat.JPEG, DEFAULT_COMPRESS_QUALITY, out);
-				} catch (FileNotFoundException e) {
-					Log.e(TAG, "Cache file could not be created.", e);
-				} finally {
-					if (out != null) {
-						try {
-							out.close();
-						} catch (IOException e) {}
-					}
-				}
-			}
-		}
-	}
-	
-	public static void writeStreams(InputStream in, OutputStream out) throws IOException {
-		byte[] buffer = new byte[1024];
-		for (int len; (len = in.read(buffer)) != -1;) {
-			out.write(buffer, 0, len);
-		}
-	}
-
-	@Override
-	public Bitmap get(String key) {
-		Bitmap bitmap = null;
-		final String diskKey = hashKeyForDisk(key);
-		synchronized(mLock) {
-            File cacheFile = new File(mCacheDir, diskKey);
-            if (cacheFile.exists()) {
-                bitmap = BitmapFactory.decodeFile(cacheFile.getPath());
+	private void init() {
+        synchronized (mLock) {
+            if (mCache == null || mCache.isClosed()) {
+                if (!mCacheDir.exists()) {
+                    mCacheDir.mkdirs();
+                }
+                final long size = calculateDiskCacheSize(mCacheDir);
+                if (size > 0) {
+                    try {
+                        mCache = DiskLruCache.open(mCacheDir, 1, 1, size);
+                    } catch (final IOException e) {
+                        Log.e(TAG, "init - " + e);
+                    }
+                }
             }
-		}
-		return bitmap;
+            mStarting = false;
+            mLock.notifyAll();
+        }
+    }
+
+    @Override
+	public void set(String data, Bitmap bitmap) {
+        synchronized (mLock) {
+            // Add to disk cache
+            if (mCache != null) {
+                final String key = hashKeyForDisk(data);
+                OutputStream out = null;
+                try {
+                    DiskLruCache.Snapshot snapshot = mCache.get(key);
+                    if (snapshot == null) {
+                        final DiskLruCache.Editor editor = mCache.edit(key);
+                        if (editor != null) {
+                            out = editor.newOutputStream(INDEX);
+                            bitmap.compress(COMPRESS_FORMAT, COMPRESS_QUALITY, out);
+                            editor.commit();
+                            out.close();
+                        }
+                    } else {
+                        snapshot.getInputStream(INDEX).close();
+                    }
+                } catch (final IOException e) {
+                    Log.e(TAG, "set - " + e);
+                } catch (Exception e) {
+                    Log.e(TAG, "set - " + e);
+                } finally {
+                    try {
+                        if (out != null) {
+                            out.close();
+                        }
+                    } catch (IOException e) {}
+                }
+            }
+        }
 	}
-	
-	public boolean delete(String key) {
-		final String diskKey = hashKeyForDisk(key);	
-		synchronized(mLock) {
-			if (createDirIfNeeded()) {
-				File cacheFile = new File(mCacheDir, diskKey);
-				return cacheFile.delete();
-			}
-		}
-		return false;
+
+	@Override
+	public Bitmap get(String data) {
+		final String key = hashKeyForDisk(data);
+        Bitmap bitmap = null;
+
+        synchronized (mLock) {
+            while (mStarting) {
+                try {
+                    mLock.wait();
+                } catch (InterruptedException e) {}
+            }
+            if (mCache != null) {
+                InputStream inputStream = null;
+                try {
+                    final DiskLruCache.Snapshot snapshot = mCache.get(key);
+                    if (snapshot != null) {
+                        inputStream = snapshot.getInputStream(INDEX);
+                        if (inputStream != null) {
+                            bitmap = BitmapFactory.decodeStream(inputStream);
+                        }
+                    }
+                } catch (final IOException e) {
+                    Log.e(TAG, "get - " + e);
+                } finally {
+                    try {
+                        if (inputStream != null) {
+                            inputStream.close();
+                        }
+                    } catch (IOException e) {}
+                }
+            }
+            return bitmap;
+        }
 	}
 
 	@Override
 	public void clear() {
-		synchronized (mLock) {
-			if (createDirIfNeeded()) {
-				for (File file : mCacheDir.listFiles()) {
-					file.delete();
-				}
-			}
-		}
+        new CacheAsyncTask().execute(MESSAGE_CLEAR);
 	}
-	
+
+    private void clearInternal() {
+        synchronized (mLock) {
+            mStarting = true;
+            if (mCache != null && !mCache.isClosed()) {
+                try {
+                    mCache.delete();
+                } catch (IOException e) {
+                    Log.e(TAG, "clear - " + e);
+                }
+                mCache = null;
+                init();
+            }
+        }
+    }
+
+    public void flush() {
+        new CacheAsyncTask().execute(MESSAGE_FLUSH);
+    }
+
+    private void flushInternal() {
+        synchronized (mLock) {
+            if (mCache != null) {
+                try {
+                    mCache.flush();
+                } catch (IOException e) {
+                    Log.e(TAG, "flush - " + e);
+                }
+            }
+        }
+    }
+
+    public void close() {
+        new CacheAsyncTask().execute(MESSAGE_CLOSE);
+    }
+
+    private void closeInternal() {
+        synchronized (mLock) {
+            if (mCache != null) {
+                try {
+                    if (!mCache.isClosed()) {
+                        mCache.close();
+                        mCache = null;
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "close - " + e);
+                }
+            }
+        }
+    }
+
 	/**
      * A hashing method that changes a string (like a URL) into a hash suitable
      * for using as a disk filename.
-     * 
+     *
      * @param key The key used to store the file
      */
-    public static String hashKeyForDisk(String key) {
+    private static String hashKeyForDisk(String key) {
         String cacheKey;
         try {
             final MessageDigest digest = MessageDigest.getInstance("MD5");
@@ -139,7 +218,7 @@ public class DiskCache implements Cache {
 
     /**
      * http://stackoverflow.com/questions/332079
-     * 
+     *
      * @param bytes The bytes to convert.
      * @return A {@link String} converted from the bytes of a hashable key used
      *         to store a filename on the disk, to hex digits.
@@ -158,12 +237,58 @@ public class DiskCache implements Cache {
 
 	@Override
 	public int maxSize() {
-		return 1;
+        synchronized (mLock) {
+            if (mCache != null && !mCache.isClosed()) {
+                return (int) mCache.getMaxSize();
+            }
+        }
+        return 0;
 	}
 
 	@Override
 	public int size() {
-		return 0; // Infinite size
+        synchronized (mLock) {
+            if (mCache != null && !mCache.isClosed()) {
+                return (int) mCache.size();
+            }
+        }
+		return 0;
 	}
+
+    private static long calculateDiskCacheSize(File dir) {
+        long size = MIN_SIZE;
+
+        try {
+            StatFs statFs = new StatFs(dir.getAbsolutePath());
+            long available = ((long) statFs.getBlockCount()) * statFs.getBlockSize();
+            // Target 10% of the total space.
+            size = available / 10;
+        } catch (IllegalArgumentException ignored) {
+        }
+
+        // Bound inside min/max size for disk cache.
+        return Math.max(Math.min(size, MAX_SIZE), MIN_SIZE);
+    }
+
+    private class CacheAsyncTask extends AsyncTask<Integer, Void, Void> {
+        @Override
+        protected Void doInBackground(Integer... params) {
+            switch (params[0]) {
+                case MESSAGE_CLEAR:
+                    clearInternal();
+                    break;
+                case MESSAGE_INIT:
+                    init();
+                    break;
+                case MESSAGE_FLUSH:
+                    flushInternal();
+                    break;
+                case MESSAGE_CLOSE:
+                    closeInternal();
+                    break;
+            }
+            return null;
+        }
+    }
 
 }
--- src/za/jamie/soundstage/utils/AppUtils.java
+++ src/za/jamie/soundstage/utils/AppUtils.java
@@ -14,10 +14,10 @@ import android.util.DisplayMetrics;
 import java.io.File;
 
 public final class AppUtils {
-	
+
 	/**
      * Get a usable cache directory (external if available, internal otherwise)
-     * 
+     *
      * @param context The {@link Context} to use
      * @param uniqueName A unique directory name to append to the cache
      *            directory
@@ -38,25 +38,25 @@ public final class AppUtils {
 
         return new File(path, uniqueName);
     }
-	
+
 	public static final void loadActionBarTabs(final ActionBar actionBar,
                                                final ViewPager viewPager) {
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		
+
 		// Create a simple listener for the tabs
 		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
 			@Override
 			public void onTabUnselected(Tab tab, FragmentTransaction ft) { }
-			
+
 			@Override
 			public void onTabSelected(Tab tab, FragmentTransaction ft) {
-				viewPager.setCurrentItem(tab.getPosition());				
+				viewPager.setCurrentItem(tab.getPosition());
 			}
-			
+
 			@Override
 			public void onTabReselected(Tab tab, FragmentTransaction ft) { }
 		};
-		
+
 		// Get all the tab titles from the viewpager's adapter
 		PagerAdapter pagerAdapter = viewPager.getAdapter();
 		for (int i = 0; i < pagerAdapter.getCount(); i++) {
@@ -64,27 +64,27 @@ public final class AppUtils {
 					.setText(pagerAdapter.getPageTitle(i))
 					.setTabListener(tabListener));
 		}
-		
+
 		// Set a listener for the viewpager to update current tab
 		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 			@Override
 			public void onPageSelected(int position) {
 				actionBar.setSelectedNavigationItem(position);
 			}
-			
+
 			@Override
 			public void onPageScrollStateChanged(int state) { }
 
 			@Override
-			public void onPageScrolled(int position, float positionOffset, 
+			public void onPageScrolled(int position, float positionOffset,
 					int positionOffsetPixels) { }
 		});
 	}
-	
+
 	public static boolean isLandscape(Resources res) {
 		return res.getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
 	}
-	
+
 	public static boolean isPortrait(Resources res) {
 		return res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
 	}
